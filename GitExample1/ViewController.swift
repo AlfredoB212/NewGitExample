06f56e081a112d59f45ac805b4bf357c60bd77ce
AlfredoB212 <alfredobarragan@Pursuits-MacBook-Pro-93.local>
2019-02-08 10:54:50 -0500

    first commit

---
+++ GitExample1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample1
+//
+//  Created by Alfredo Barragan on 2/8/19.
+//  Copyright © 2019 Alfredo Barragan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("doing github things")
+    }
+
+
+}
+
